0ff0cd66d654acc3be55704f9b8173189e34d5c7
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-12-02 20:18:08 +0900

    add:Clockwork

--- notify.rb
+++ notify.rb
@@ -2,9 +2,17 @@ require "slack-notify"
 require 'clockwork'
 require 'dotenv'
 Dotenv.load
+include Clockwork
 
 client = SlackNotify::Client.new(
   webhook_url: ENV['WEBHOOK']
 )
 
-client.notify("Hello There!")
+client.notify("こんにちは！(Rubyで投稿)")
+
+module Clockwork
+  every(3.minutes, 'test') do
+  client = SlackNotify::Client.new(webhook_url: ENV['WEBHOOK'])
+  client.notify("3分ごとに自動で送信します!")
+  end
+end
